49def9bd24f6932e4e2d6b48f8ceadc440029712
Priti Kumari <pritikumarij242001@gmail.com>
2025-05-26 17:40:33 +0530

    hhh

--- src/components/ARViewer.jsx
+++ src/components/ARViewer.jsx
@@ -1,9 +1,59 @@
+// import React from "react";
+
+// const ARViewer = () => {
+//   return (
+//     <>
+//       {/* Hidden HTML video element to be used as a texture */}
+//       <video
+//         id="myvid"
+//         src="https://res.cloudinary.com/dsouwco6y/video/upload/fl_attachment/103224-662114480_tiny_i37zoh.mp4"
+//         loop
+//         muted
+//         preload="auto"
+//         crossOrigin="anonymous"
+//         style={{ display: "none" }}
+//       ></video>
+
+//           <a-scene embedded arjs="sourceType: webcam; facingMode: environment;">
+//         {/* Marker */}
+//         <a-marker preset="hiro">
+//           {/* A-Frame video element using HTML video as texture */}
+//           <a-video
+//             src="#myvid"
+//             width="2"
+//             height="1"
+//             position="0 0.5 0"
+//             rotation="-90 0 0"
+//             autoplay
+//             loop
+//             muted
+//           ></a-video>
+//         </a-marker>
+
+//         {/* Camera */}
+//         <a-entity camera></a-entity>
+//       </a-scene>
+//     </>
+//   );
+// };
+
+// export default ARViewer;
 import React from "react";
 
 const ARViewer = () => {
   return (
     <>
-      {/* Hidden HTML video element to be used as a texture */}
+      {/* Marker image so user can scan */}
+      <div style={{ textAlign: "center", marginBottom: "20px" }}>
+        <h3>Scan the marker below with your phone camera</h3>
+        <img
+          src="https://raw.githubusercontent.com/AR-js-org/AR.js/master/three.js/data/images/HIRO.jpg"
+          alt="HIRO Marker"
+          style={{ width: "300px" }}
+        />
+      </div>
+
+      {/* Hidden video texture */}
       <video
         id="myvid"
         src="https://res.cloudinary.com/dsouwco6y/video/upload/fl_attachment/103224-662114480_tiny_i37zoh.mp4"
@@ -14,10 +64,8 @@ const ARViewer = () => {
         style={{ display: "none" }}
       ></video>
 
-          <a-scene embedded arjs="sourceType: webcam; facingMode: environment;">
-        {/* Marker */}
+      <a-scene embedded arjs="sourceType: webcam; facingMode: environment;">
         <a-marker preset="hiro">
-          {/* A-Frame video element using HTML video as texture */}
           <a-video
             src="#myvid"
             width="2"
@@ -29,8 +77,6 @@ const ARViewer = () => {
             muted
           ></a-video>
         </a-marker>
-
-        {/* Camera */}
         <a-entity camera></a-entity>
       </a-scene>
     </>
